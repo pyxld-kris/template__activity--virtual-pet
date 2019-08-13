92c397f37dd077046c3e6592e40335dc914b9c4a
Kris Gano <46331884+pyxld-kris@users.noreply.github.com>
2019-08-12 21:45:40 -0500

    Added ability to create x number of random pos food items in modify.js

--- modify.mjs
+++ modify.mjs
@@ -1,5 +1,22 @@
 /* eslint-disable */
-let scene = this;
+let scene = this; // Setting this variable for readability
+
+// How many food items should we give to our pet?
+var NUM_FOODS = 3; // Integer variable
+
+// This function creates one food item in our active game scene
+function createFood() {
+  let thisMeat = scene.physics.add.sprite(60+Math.floor(Math.random()*60), 10, 'meat');
+  scene.physics.add.collider(thisMeat, scene.ground);
+  scene.physics.add.collider(thisMeat, scene.pet.sprite, function() {
+    thisMeat.destroy();
+  });
+}
+
+// Loop the appropriate number of times defined by NUM_FOODS
+for (let i=0; i<NUM_FOODS; i++) {
+  createFood();
+}
 
 /*
 var PET_NAME = "Pet"; // String variable
